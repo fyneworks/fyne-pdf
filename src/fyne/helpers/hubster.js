5061d94570954e1de02702ddd9588eb0bfba34f6
Diego Monte Alto <diego@fyneworks.com>
2020-07-23 08:51:57 +0100

    Initial commit

---
+++ src/fyne/helpers/hubster.js
@@ -0,0 +1,32 @@
+export { fyneHub } from '@fyne/ui/hubster';
+export { fyneApp } from '@fyne/ui/hubster';
+
+//import { context } from '@fyne/ui/context';
+//import { createHub } from 'hubster-js'
+
+//import { createHub } from 'hubster-js'
+//export const Hub = window.Hub = createHub(config);
+//
+//export const fyneHub = createHub;
+//
+//export const fyneAppConfig = ({app_id,ele_id,file}) => {
+//    ele_id = ele_id || (ele_id || (app_id+'-root'));
+//    return {
+//        name: app_id,
+//        id: app_id,
+//        el: {
+//            type: 'div',
+//            attrs: [{ type: 'id', value: ele_id }],
+//            sel: '#'+ele_id
+//        },
+//        global_dependencies: ['react', 'react-dom'],
+//        url: context.APP_BASE + (file || (app_id+'.js'))
+//    }
+//};
+//
+//export const fyneApp = app_id => {
+//    console.log('fyneApp', {context,app_id});
+//    const app = fyneAppConfig({app_id,file:app_id+'.js'});
+//    console.log('fyneApp', {context,app_id,app});
+//    return app;
+//}
